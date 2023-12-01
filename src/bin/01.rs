de1e5edfcc1d054e45df9deb4d1b6e81850ebe8e
Jan Wilken Dörrie <jwdoerrie@gmail.com>
2023-12-01 10:11:57 +0100

    Day 1B

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use itertools::Itertools;
 
 advent_of_code::solution!(1);
@@ -13,7 +15,42 @@ pub fn part_one(input: &str) -> Option<u32> {
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    let numbers = HashMap::from([
+        ("0", 0),
+        ("1", 1),
+        ("2", 2),
+        ("3", 3),
+        ("4", 4),
+        ("5", 5),
+        ("6", 6),
+        ("7", 7),
+        ("8", 8),
+        ("9", 9),
+        ("one", 1),
+        ("two", 2),
+        ("three", 3),
+        ("four", 4),
+        ("five", 5),
+        ("six", 6),
+        ("seven", 7),
+        ("eight", 8),
+        ("nine", 9),
+    ]);
+
+    let mut total_sum = 0;
+    for line in input.lines() {
+        let first_digit = numbers
+            .iter()
+            .filter_map(|(k, v)| (line.find(k).map(|i| (i, v))))
+            .min();
+        let last_digit = numbers
+            .iter()
+            .filter_map(|(k, v)| (line.rfind(k).map(|i| (i, v))))
+            .max();
+        total_sum += first_digit.unwrap().1 * 10 + last_digit.unwrap().1;
+    }
+
+    Some(total_sum)
 }
 
 #[cfg(test)]
@@ -29,6 +66,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(281));
     }
 }
