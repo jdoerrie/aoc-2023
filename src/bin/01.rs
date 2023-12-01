7bbae2b63dcbd50c8490e6ea685598e350747541
Jan Wilken Dörrie <jwdoerrie@gmail.com>
2023-12-01 09:40:55 +0100

    Day 1A

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -1,7 +1,15 @@
+use itertools::Itertools;
+
 advent_of_code::solution!(1);
 
 pub fn part_one(input: &str) -> Option<u32> {
-    None
+    Some(
+        input
+            .lines()
+            .map(|line| line.chars().filter_map(|c| c.to_digit(10)).collect_vec())
+            .map(|v| v.first().unwrap_or(&0) * 10 + v.last().unwrap_or(&0))
+            .sum(),
+    )
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
@@ -15,7 +23,7 @@ mod tests {
     #[test]
     fn test_part_one() {
         let result = part_one(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(142));
     }
 
     #[test]
