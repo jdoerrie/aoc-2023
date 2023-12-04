dea33972a4b4f74e07193f701fe999d6d39604dd
Jan Wilken Dörrie <jwdoerrie@gmail.com>
2023-12-04 08:56:29 +0100

    Day 4B

--- src/bin/04.rs
+++ src/bin/04.rs
@@ -29,7 +29,19 @@ pub fn part_one(input: &str) -> Option<u32> {
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    let mut counts = vec![0; input.lines().count()];
+    for (i, line) in input.lines().enumerate() {
+        counts[i] += 1;
+        let (winning_numbers, scratch_card) =
+            line.split_once(": ").unwrap().1.split_once(" | ").unwrap();
+        let matching_nums = split_into_set(winning_numbers)
+            .intersection(&split_into_set(scratch_card))
+            .count();
+        for j in i + 1..=i + matching_nums {
+            counts[j] += counts[i];
+        }
+    }
+    Some(counts.iter().sum())
 }
 
 #[cfg(test)]
@@ -45,6 +57,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(30));
     }
 }
