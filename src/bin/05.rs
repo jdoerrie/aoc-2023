6dcec79e3c907c1026886943d1f4c1cb49cd7988
Jan Wilken Dörrie <jwdoerrie@gmail.com>
2023-12-05 10:05:07 +0100

    Day 5B

--- src/bin/05.rs
+++ src/bin/05.rs
@@ -11,10 +11,27 @@ fn parse_nums(line: &str) -> Vec<i64> {
         .collect_vec()
 }
 
-pub fn part_one(input: &str) -> Option<i64> {
-    let seeds = parse_nums(input.lines().next().unwrap().split_once(": ").unwrap().1);
+fn parse_seeds(line: &str) -> Vec<i64> {
+    parse_nums(line.split_once(": ").unwrap().1)
+}
 
-    let maps = input
+fn parse_seed_ranges(line: &str) -> Vec<Range<i64>> {
+    parse_nums(line.split_once(": ").unwrap().1)
+        .into_iter()
+        .chunks(2)
+        .into_iter()
+        .map(|c| {
+            let (start, size) = c.collect_tuple().unwrap();
+            Range {
+                start,
+                end: start + size,
+            }
+        })
+        .collect_vec()
+}
+
+fn parse_maps(input: &str) -> Vec<RangeMap<i64, i64>> {
+    input
         .lines()
         .skip(1)
         .group_by(|l| l.is_empty())
@@ -34,7 +51,12 @@ pub fn part_one(input: &str) -> Option<i64> {
             }));
             map
         })
-        .collect_vec();
+        .collect_vec()
+}
+
+pub fn part_one(input: &str) -> Option<i64> {
+    let seeds = parse_seeds(input.lines().next().unwrap());
+    let maps = parse_maps(input);
 
     Some(
         *(maps
@@ -50,8 +72,26 @@ pub fn part_one(input: &str) -> Option<i64> {
     )
 }
 
-pub fn part_two(input: &str) -> Option<u32> {
-    None
+pub fn part_two(input: &str) -> Option<i64> {
+    let seeds = parse_seed_ranges(input.lines().next().unwrap());
+    let maps = parse_maps(input);
+
+    maps.iter()
+        .fold(seeds.clone(), |acc, map| {
+            acc.into_iter()
+                .flat_map(|seeds| {
+                    map.gaps(&seeds)
+                        .chain(map.overlapping(&seeds).map(|(overlap, &offset)| Range {
+                            start: seeds.start.max(overlap.start) + offset,
+                            end: seeds.end.min(overlap.end) + offset,
+                        }))
+                        .collect_vec()
+                })
+                .collect_vec()
+        })
+        .iter()
+        .map(|rng| rng.start)
+        .min()
 }
 
 #[cfg(test)]
@@ -67,6 +107,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(46));
     }
 }
